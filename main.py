9ec2329d0abe88f0d7bcd472312a9491b8a06992
emads22 <emadsaab722@gmail.com>
2024-05-25 13:35:38 +0300

    create and test the function to find closest answer or most similar sentence

--- main.py
+++ main.py
@@ -76,22 +76,34 @@ def find_closest_answer(text, question, similarity_threshold=0.3):
     return closest_answer
 
 
-def main():
+def testing_function():
+    """
+    A function to test finding the closest matching answer to a question within a given text.
+
+    Args:
+        None
+
+    Returns:
+        None
+    """
+
     # Original text and question
-    text = 'Originally, vegetables were collected from the wild by hunter-gatherers. Vegetables are all plants. Vegetables can be eaten either raw or cooked.'
-    question = 'What are vegetables?'
+    text = """
+A yacht is a luxurious watercraft typically used for recreational purposes, often associated with leisurely sailing or cruising. These vessels are usually privately owned and can range in size from small, sleek sailing yachts to large, extravagant motor yachts equipped with modern amenities such as spacious cabins, gourmet kitchens, and entertainment systems. Yachts are commonly seen in coastal areas and are popular among the affluent for hosting parties, enjoying vacations, or simply basking in the serenity of the open sea.
+"""
+    question = 'Where are yachts seen?'
 
     # Find the closest matching answer to the question in the provided text
     answer = find_closest_answer(text, question)
 
     # Print the original text
-    print(f'\n- Input text is:\n=>', text)
+    print(f'\n\n- Input text is:\n\n=>', text)
 
     # Print the question
-    print(f'\n- Question is:\n=>', question)
+    print(f'\n\n- Question is:\n\n=>', question)
 
     # Print the closest answer to the question from the provided text
-    print(f'\n- The closest answer to the question from the provided text is:\n=>', end=" ")
+    print(f'\n\n- The closest answer to the question from the provided text is:\n\n=>', end=" ")
 
     # Check if a matching answer was found
     if answer is None:
@@ -101,8 +113,8 @@ def main():
         # Print the closest matching answer
         print(answer)
 
-    print()
+    print("\n")
 
 
 if __name__ == "__main__":
-    main()
+    testing_function()
